6148919cd742c3b0b75261ca3cb4e9ac6c1326cc
Ole Begemann <ole@oleb.net>
2024-03-14 16:26:48 +0100

    Add GCD-based "workaround"

--- Sources/App.swift
+++ Sources/App.swift
@@ -29,6 +29,38 @@ func performWork() async throws {
     }
 }
 
+/// Alternative implementation that "fixes" the deadlock
+/// until you create so many child tasks that you exhaust the GCD limit.
+func performWorkUsingGCD() async throws {
+    let imageURL = findResourceInBundle("church-of-the-king-j9jZSqfH5YI-unsplash.jpg")!
+    try await withThrowingTaskGroup(of: (id: Int, faceCount: Int).self) { group in
+        // This "fixes" the deadlock at the cost of thread explosion.
+        // Also, GCD's max thread pool size is 64, so if you increase to 64 or
+        // more child tasks it will deadlock again.
+        for i in 1...64 {
+            group.addTask {
+                print("Task \(i) starting")
+                return try await withUnsafeThrowingContinuation { c in
+                    DispatchQueue.global().async {
+                        do {
+                            let request = VNDetectFaceRectanglesRequest()
+                            let requestHandler = VNImageRequestHandler(url: imageURL)
+                            try requestHandler.perform([request])
+                            let faces = request.results ?? []
+                            c.resume(returning: (id: i, faceCount: faces.count))
+                        } catch {
+                            c.resume(throwing: error)
+                        }
+                    }
+                }
+            }
+        }
+        for try await (id, faceCount) in group {
+            print("Task \(id) detected \(faceCount) faces")
+        }
+    }
+}
+
 func findResourceInBundle(_ filename: String) -> URL? {
     // The Bundle.module bundle has a different structure, depending on whether
     // you build the program with SwiftPM (`swift build`) or with Xcode.
